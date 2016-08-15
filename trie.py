3cf05299fab54d6d46ba6903b9ae976cb36ef633
Champ Somsuk <champ@exablox.com>
2016-08-14 21:49:33 -0700

    update trie

--- trie.py
+++ trie.py
@@ -1,45 +1,68 @@
-class Node(object):
-	def __init__(self, data):
-		self.data = data
-		self.childs = {}
+class Node:
 
-	def __repr__(self):
-		return 'Node {}: {}'.format(self.data, self.childs)
+    def __init__(self):
+        self.word = None  # only assigned if this node is last letter of word
+        self.childs = {}
 
-	def get_child(self, data):
-		return self.childs.get(data)
+    def __add__(self, word, str_pos=0):
 
-	def set_child(self, data):
-		if not self.childs.get(data):
-			self.childs[data] = Node(data)
-		return self.childs[data]
+        cur = word[str_pos]
+        node = self.childs.get(cur)
+        if not node:
+            node = Node()
+            self.childs[cur] = node
+        if str_pos + 1 == len(word):
+            node.word = word
+        else:
+            node.__add__(word, str_pos + 1)
 
-	def add_word(self, word):
-		node = self
-		for i in xrange(len(word)):
-			node = node.set_child(word[i])
+    def __repr__(self):
+        return self.word
 
-	def walk_node(self, word):
-		node = self
-		for i in xrange(len(word)):
-			node = node.get_child(word[i])
-			if not node:
-				return
-		return node
+    def __get_all__(self):
+        out = []
+        if self.word:
+        	out.append(self.word)
+        for c, node in self.childs.iteritems():
+        	out += node.__get_all__()
+        return out
 
-	def predict(self):
-		""" find path from node to all leafs
-		"""
-		pass
+    def __get_all_with_prefix__(self, prefix):
+        """ walk until the node prefix
+        	- get_all
+        """
+        node = self
+        for c in prefix:
+        	node = node.childs.get(c)
+        	if not node:
+        		return
+        return node.__get_all__()
 
+class Trie:
 
+    def __init__(self):
+        self.root = Node()
 
+    def insert(self, word):
+        self.root.__add__(word)
 
-if __name__ == '__main__':
+    def get_all(self):
+    	return self.root.__get_all__()
+    def get_all_with_prefix(self, prefix):
+    	return self.root.__get_all_with_prefix__(prefix)
 
-	root = Node('root')
-	for word in ['care', 'cars', 'carp', 'carb', 'craw', 'crab']:
-		root.add_word(word)
-	print root
+possible_words = ['cars', 'care', 'workers', 'cattles', 'emergency', 'carb', 'working', 'worship', 'worry', 'woraya']
 
-	# suggestion
+trie = Trie()
+trie.insert("go")
+trie.insert("gone")
+trie.insert("gi")
+trie.insert("cool")
+trie.insert("comb")
+trie.insert("grasshopper")
+trie.insert("home")
+trie.insert("hope")
+trie.insert("hose")
+for word in possible_words:
+	trie.insert(word)
+print trie.get_all()
